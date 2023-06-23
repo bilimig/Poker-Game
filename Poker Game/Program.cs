6d4ebc3d963362a56412b27f805cdf549d08d9ad
Owczarek Kamil <bilimig@wp.pl>
2023-06-23 19:54:03 +0200

    Added PokerGame class

--- Poker Game/Program.cs
+++ Poker Game/Program.cs
@@ -205,9 +205,85 @@ public class Round
         }
     }
 
+    public class PokerGame
+    {
+        private List<Player> players;
+        private int currentPlayerIndex;
+        private int roundNumber;
+        private Round currentRound;
 
+    public PokerGame(List<Player> players)
+        {
+            this.players =players;
+            currentPlayerIndex = 0;
+            roundNumber = 1;
+            currentRound = null;
+        }
+
+    public void Start()
+        {
+            Console.WriteLine("Rozpoczyna się gra w pokera!");
+            Console.WriteLine("=================================");
+            Console.WriteLine();
+            while (!IsGameOver())
+            {
+                Console.WriteLine("Runda numer: " + roundNumber);
+                Console.WriteLine("--------------------------");
+                Console.WriteLine();
+
+                StartNewRound();
+
+                // Sprawdzenie warunków końca gry
+                if (IsGameOver())
+                {
+                    break;
+                }
+
+                // Przejście do następnego gracza
+                currentPlayerIndex = (currentPlayerIndex + 1) % players.Count;
+                roundNumber++;
+
+                // dodac ruchy itd
+            }
+
+            // Wyświetlenie zwycięzcy
+            Player winner = DetermineWinner();
+            Console.WriteLine("=================================");
+            Console.WriteLine("Gra zakończona! Zwycięzca: " + winner.Name);
+
+
+
+        }
+        private void StartNewRound()
+        {
+            Console.WriteLine("Nowa runda:");
+            Console.WriteLine();
+
+            currentRound = new Round(players);
+            currentRound.Start();
+
+           // dodanie obslugi zakladow podejmowanie decyzji itd
+        }
+
+        private bool IsGameOver()
+        {
+
+            return false;
+        }
+
+        private Player DetermineWinner()
+        {
+            
+           
+            // stworzyc how to win
+
+            return players[0]; 
+        }
+
+    }
 
 }
+
 
 
 
